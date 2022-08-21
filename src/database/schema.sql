9a888a8b1479d5134c84d922aaabba4ffd85af3f
Luis Fernando <luisfernandogvv@gmail.com>
2022-08-21 00:51:40 -0300

    Criando Schema SQL: Tabela de filmes

---
+++ src/database/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS movies (
+
+    movie_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    movie_name VARCHAR NOT NULL,
+    movie_category VARCHAR NOT NULL
+    movie_year DATE
+);
