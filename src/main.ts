9fc29bc25e838100a4bf88af23715136def8215e
Luis Fernando <luisfernandogvv@gmail.com>
2022-08-23 18:03:35 -0300

    Atualizando Main.Ts e Procfile

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('documentation', app, document);
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
