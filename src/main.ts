7a37d2f9507673bcf8fb62625a59d3b15b345fa1
Luis Fernando <luisfernandogvv@gmail.com>
2022-08-23 20:13:44 -0300

    Ativando Cors

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
+  app.enableCors();
   SwaggerModule.setup('documentation', app, document);
   await app.listen(process.env.PORT || 5000);
 }
